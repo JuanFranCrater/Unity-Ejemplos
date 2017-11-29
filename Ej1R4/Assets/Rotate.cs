5722182e0788b9873dc8cd9c88938d31f06e19fc
JuanFranCrater <juanfbenitezlopez@gmail.com>
2017-11-29 18:56:45 +0100

    Avance en billar

---
+++ Ej1R4/Assets/Rotate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+	Rigidbody _rigidBody;
+	public float _fuerza = 10F;
+	void Start()
+	{
+		_rigidBody = GetComponent<Rigidbody> ();
+	}
+	void FixedUpdate () {
+		if(Input.GetKey(KeyCode.A))
+		{
+			transform.Rotate (Vector3.up);
+		}
+		if(Input.GetKey(KeyCode.D))
+		{
+			transform.Rotate (Vector3.down);
+		}
+		if (Input.GetKey (KeyCode.Space)) {
+			_rigidBody.AddForce(Vector3.forward*_fuerza, ForceMode.Impulse);
+		}
+	}
+}
