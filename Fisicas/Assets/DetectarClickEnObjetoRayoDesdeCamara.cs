9d487fe886509c6a9105d52c6cf1e310674ddef9
JuanFranCrater <juanfbenitezlopez@gmail.com>
2017-12-01 08:51:20 +0100

    ya es diciembre

---
+++ Fisicas/Assets/DetectarClickEnObjetoRayoDesdeCamara.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectarClickEnObjetoRayoDesdeCamara : MonoBehaviour {
+
+	Ray _pulsacion;
+	RaycastHit _colision; //Almacena Informacion
+
+
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+		if(Input.GetMouseButton(0))//boton izquierdo:0 boton derecho: 1 boton central: 2
+		{
+			_pulsacion = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast (_pulsacion,out _colision)) {
+				
+				_colision.collider.gameObject.GetComponent<Transform>().Rotate(new Vector3(0.5F,0.5F,0.5F));
+
+
+			}
+		}
+
+	}
+}
---
+++ Fisicas/Assets/EsferaCast.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EsferaCast : MonoBehaviour {
+
+	Vector3 _posicionInicial;
+	float _radioEsfera;
+	Vector3 _direccion;
+	RaycastHit _hitInfo;
+	float _distancia;
+
+
+	void Start () {
+		_radioEsfera = transform.localScale.x / 2F;
+		_direccion = Vector3.right;
+		_distancia = 100;
+	}
+	
+
+	void Update () {
+		/*
+		personaje = gameObject.GetComponent<CharacterController> ();
+		*/
+		_posicionInicial = transform.position;
+		if(Physics.SphereCast(_posicionInicial,_radioEsfera,_direccion,out _hitInfo,_distancia))
+		{
+			/*
+			_hitInfo.collider.gameObject.GetComponent<Renderer>().enabled = false;
+			*/
+			Debug.Log ("Detectado obstaculo a " + _hitInfo.collider.name );
+			Debug.DrawRay (_posicionInicial, _direccion, Color.red);
+		}
+	}
+}
